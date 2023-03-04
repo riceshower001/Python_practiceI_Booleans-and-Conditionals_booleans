ac65d7e2a246d8082bdf0bfb7e45d64a26a3cdd6
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-04 14:18:35 +0900

    Add files via upload

---
+++ Python_practiceI_Booleans-and-Conditionals_booleans.py
@@ -0,0 +1,10 @@
+x = 10
+# Please print "x is greater than 30." if x is greater than 30.
+if x > 30 :
+  print("x is greater than 30.")
+
+money = 500
+apple_price = 200
+# If the value of "money" is greater than or equal to the value of "apple_price", print "You can buy apples.".
+if money >= apple_price :
+  print("You can buy apples.")
